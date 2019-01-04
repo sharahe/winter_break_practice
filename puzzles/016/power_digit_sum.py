b883da9f0bf9f7256c0a1618ccde6928e3decd82
sharahe <43322456+sharahe@users.noreply.github.com>
2019-01-04 14:04:07 -0500

    Update power_digit_sum.py

--- puzzles/016/power_digit_sum.py
+++ puzzles/016/power_digit_sum.py
@@ -1,3 +1,8 @@
-"""215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+"""2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?"""
+What is the sum of the digits of the number 2**1000?"""
+num = 2**1000
+sum = 0
+for i in range(0, len(str(num))):
+    sum = sum + int(str(num)[i])
+print("Sum of digits: {}".format(sum))
